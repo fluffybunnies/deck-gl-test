140310a90323c668262d2af416c5178eaf274f82
alec <alec.hulce@gmail.com>
2017-06-27 19:20:38 -0700

    Fix sample data - viewport didnt include data points

--- app/components/Map.js
+++ app/components/Map.js
@@ -14,23 +14,22 @@ export default React.createClass({
 	}
 
 	,render: function(){
-		// sample init data from https://uber.github.io/deck.gl/#/documentation/getting-started/using-with-react
 		var viewport = {
 			width: 500,
 			height: 500,
-			longitude: -100,
-			latitude: 40.7,
-			zoom: 3,
-			pitch: 0,
-			bearing: 0
+			latitude: 37.785164,
+			longitude: -122.41669,
+			zoom: 16.140440,
+			bearing: -20.55991,
+			pitch: 0
 		}
 		var data = [
 			{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}
 		]
 		//return <div>Hi! Im a map</div>
 		return (<div className="replace-me-with-static-mapgl" style={{width:viewport.width+'px',height:viewport.height+'px',background:'#333'}}>
-			<DeckGL {...viewport} layers={[
-				new LineLayer({id: 'line-layer', data})
+			<DeckGL debug {...viewport} layers={[
+				new LineLayer({ id:'line-layer', data:data })
 			]} />
 		</div>)
 	}
